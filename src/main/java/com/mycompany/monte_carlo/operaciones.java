16bde987e99cb196686fbe52c0cc3bcb339e2262
DieGordito <diegoluflex@gmail.com>
2021-04-27 17:50:05 -0500

    Cambios agregados

--- src/main/java/com/mycompany/monte_carlo/operaciones.java
+++ src/main/java/com/mycompany/monte_carlo/operaciones.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class operaciones {
 
-    int a, b, m, res, j = 0,k=0;
-    double xi [] = new double[6];
-    double zi [] = new double[6];
+    int a,b,m,j=0,k=0,indep;
+    double xi[] = new double[6];
+    double zi[] = new double[6];
     double f[] = new double[10];
     double fx[] = new double[5];
     double y[] = new double[5];
-    double[] X = new double[5];
+    double X[] = new double[5];
     StringBuilder ecuacion = new StringBuilder();
 
     public operaciones(int a, int b, int m) {
@@ -26,28 +26,29 @@ public class operaciones {
         }
     }
 
-    public void ecuacion(String x, String z, int i) {
+    public void ecuacion(String x, String z, int indep) {
+        this.indep = indep;
         ecuacion.append(x + "x" + "^" + z);
         xi[k] = Double.parseDouble(x);
         zi[k] = Double.parseDouble(z);
-        if (i > 1) {
+        if (indep > 1) {
             ecuacion.append(" + ");
+            ecuacion.append(indep);
         }
         k++;
     }
 
-    public void X1(double uno, double dos, int i) {
+    public void X1(double uno,double dos,int i) {
         X[i] = this.a + uno * (this.b - this.a);
     }
 
-    public void Y(double uno, int dos, int i) {
+    public void Y(double uno,int dos,int i) {
         y[i] = uno * dos;
     }
 
     public void FX() {
-        fx[j] = xi[0] * Math.pow( X[j], zi[0]) + xi[1] *Math.pow( X[j], zi[1]);
+        fx[j] = xi[0] * Math.pow(X[j], zi[0]) + indep;
         j++;
-//                      2.50335                                 7.9216
     }
 
 }
